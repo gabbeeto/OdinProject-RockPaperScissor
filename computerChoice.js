a32b975d4a77bfa70426d0a8a72665392c4b05ef
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-05-30 15:48:24 -0300

    manipulate dom to make an ui

--- computerChoice.js
+++ computerChoice.js
@@ -1,98 +1,165 @@
 let humanChoice;
 
 let choice = ['rock', 'paper', 'scissor'];
-humanPoints = 0;
-computerPoints = 0;
+let humanPoints = 0;
+let computerPoints = 0;
 
 
 let playerName;
-//let playerName = prompt(`what's your name`);
 function getPlayerName(){
 
-let playerName = document.querySelector('#nameInputContainer input').value;
+playerName = document.querySelector('#nameInputContainer input').value;
 document.querySelector('#nameInputContainer').style.display = "none";
 
 document.querySelector('#humanChoiceContainer').style.display = "flex";
 }
 
 
-PlayerNameDoneButton = document.querySelector('#nameInputContainer button');
-PlayerNameDoneButton.addEventListener('click', getPlayerName)
+
+
+
 
 
 let computerChoice;
-
 function getRandomChoice() {
 computerChoice = choice[Math.round(Math.random() * 2)];
 }
 
 
 
-function rules() {
 
-if(humanChoice == 'rock' && computerChoice == 'rock'){
-alert(`it's a tie because both of you chose rock`);
+
+let message = document.getElementById('message');
+let humanPointText = document.getElementById('humanPoints');
+let computerPointText = document.getElementById('computerPoints');
+function gameRules() {
+if(humanPoints < 5 && computerPoints < 5){
+
+switch (humanChoice) {
+
+	case 'rock':
+
+
+
+
+if(computerChoice == 'rock'){
+message.innerText = `it's a tie because both of you chose rock`;
 }
 
-else if(humanChoice == 'rock' && computerChoice == 'paper'){
+else if(computerChoice == 'paper'){
 computerPoints += 1;
-alert(`computer wins because ${playerName} chose rock and computer chose 'paper'`);
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
+message.innerHTML = `duck wins because ${playerName} chose rock and duck chose 'paper'`;
 }
 
-else if(humanChoice == 'rock' && computerChoice == 'scissor'){
+else if(computerChoice == 'scissor'){
 humanPoints += 1;
-alert(`${playerName} wins because computer chose scissor and ${playerName} chose rock`);
+humanPointText.innerHTML = `human points = ${humanPoints}`;
+message.innerText = `${playerName} wins because duck chose scissor and ${playerName} chose rock`;
 }
 
+		break;
 
 
-else if(humanChoice == 'paper' && computerChoice == 'rock'){
+
+  case 'paper':
+
+
+
+if(computerChoice == 'rock'){
 humanPoints += 1;
-alert(`${playerName} wins because computer chose 'rock' and ${playerName} chose paper`);
+humanPointText.innerHTML = `human points = ${humanPoints}`;
+message.innerText = `${playerName} wins because duck chose 'rock' and ${playerName} chose paper`;
 }
 
-else if(humanChoice == 'paper' && computerChoice == 'paper'){
-alert(`it's a tie because both of you chose paper`);
+else if(computerChoice == 'paper'){
+message.innerText = `it's a tie because both of you chose paper`;
 }
 
-else if(humanChoice == 'paper' && computerChoice == 'scissor'){
+else if(computerChoice == 'scissor'){
 computerPoints += 1;
-alert(`computer wins because ${playerName} chose paper and computer chose 'rock'`);
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
+message.innerText = `Duck wins because ${playerName} chose paper and duck chose 'rock'`;
 }
+		
+		break;
+
+
+  case 'scissor':
 
 
 
-else if(humanChoice == 'scissor' && computerChoice == 'rock'){
+if(computerChoice == 'rock'){
 computerPoints += 1;
-alert(`computer wins because ${playerName} chose scissor and computer choose rock`);
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
+message.innerText = `Duck wins because ${playerName} chose scissor and duck choose rock`;
 }
 
-else if(humanChoice == 'scissor' && computerChoice == 'paper'){
+else if(computerChoice == 'paper'){
 humanPoints += 1;
-alert(`${playerName} wins because computer chose paper and ${playerName} chose scissor`);
+humanPointText.innerHTML = `human points = ${humanPoints}`;
+message.innerText = `${playerName} wins because duck chose paper and ${playerName} chose scissor`;
 }
 
-else if(humanChoice == 'scissor' && computerChoice == 'scissor'){
-alert(`it's a tie because both of you chose scissor`);
+else if(computerChoice == 'scissor'){
+message.innerText = `it's a tie because both of you chose scissor`;
+}
+
+		break;
+
+}
+}
+
+else {
+document.querySelector('#humanChoiceContainer').parentElement.removeChild(document.querySelector('#humanChoiceContainer'));
+let finalText = document.createElement('h1');
+if(humanPoints > computerPoints){
+finalText.innerText = 'human won!!!!!.. you saved this planet agaisnt this evil duck...';
+}
+else{
+finalText.innerText = 'you got defeated by the evil duck';
+}
+document.body.appendChild(finalText);
+
+
 }
 
 }
 
-//let round = 1;
-
-//while(humanPoints < 5 && computerPoints < 5){
-//humanChoice = prompt(`rock, paper or scissor?`);
-//getRandomChoice();
-//rules();
-//alert(`Round ${round} \nHuman Points: ${humanPoints}. \nComputer Points: ${computerPoints}`)
-//round += 1;
-//}
-
-//if(humanPoints > computerPoints){
-//alert(`${playerName} won`);
-//}
-//else{
-//alert(`computer won`);
-//}
 
 
+
+
+
+
+function playerChoice(e){
+console.log(e.target)
+RockPaperScissorImages = document.querySelectorAll('#invisibleContainer > *')
+switch(e.target){
+  case  RockPaperScissorImages[0]:
+	humanChoice = 'rock';
+	break;
+
+
+  case  RockPaperScissorImages[1]:
+	humanChoice = 'paper';
+	break;
+
+
+  case  RockPaperScissorImages[2]:
+	humanChoice = 'scissor';
+	break;
+
+	}
+
+  getRandomChoice();
+	gameRules();
+}
+
+let invisibleContainer = document.querySelector('#invisibleContainer');
+invisibleContainer.addEventListener('click', playerChoice);
+
+
+
+PlayerNameDoneButton = document.querySelector('#nameInputContainer button');
+PlayerNameDoneButton.addEventListener('click', getPlayerName);
