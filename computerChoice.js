52d4751c3334ec56e11d2597926a3287e2f4a080
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-05-31 14:48:17 -0300

    finish the difficulty system

--- computerChoice.js
+++ computerChoice.js
@@ -11,7 +11,9 @@ function getPlayerName(){
 playerName = document.querySelector('#nameInputContainer input').value;
 document.querySelector('#nameInputContainer').style.display = "none";
 
-document.querySelector('#humanChoiceContainer').style.display = "flex";
+document.querySelector('#difficultySystem').style.display = "flex";
+
+
 }
 
 
@@ -43,17 +45,19 @@ switch (humanChoice) {
 
 
 if(computerChoice == 'rock'){
+addUnfairPointsWhenHardDifficulty();
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerText = `it's a tie because both of you chose rock`;
 }
 
 else if(computerChoice == 'paper'){
-computerPoints += 1;
+addPointsToComputer();
 computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerHTML = `duck wins because ${playerName} chose rock and duck chose 'paper'`;
 }
 
 else if(computerChoice == 'scissor'){
-humanPoints += 1;
+addPointsToHuman();
 humanPointText.innerHTML = `human points = ${humanPoints}`;
 message.innerText = `${playerName} wins because duck chose scissor and ${playerName} chose rock`;
 }
@@ -67,17 +71,19 @@ message.innerText = `${playerName} wins because duck chose scissor and ${playerN
 
 
 if(computerChoice == 'rock'){
-humanPoints += 1;
+addPointsToHuman();
 humanPointText.innerHTML = `human points = ${humanPoints}`;
 message.innerText = `${playerName} wins because duck chose 'rock' and ${playerName} chose paper`;
 }
 
 else if(computerChoice == 'paper'){
+addUnfairPointsWhenHardDifficulty();
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerText = `it's a tie because both of you chose paper`;
 }
 
 else if(computerChoice == 'scissor'){
-computerPoints += 1;
+addPointsToComputer();
 computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerText = `Duck wins because ${playerName} chose paper and duck chose 'rock'`;
 }
@@ -90,18 +96,20 @@ message.innerText = `Duck wins because ${playerName} chose paper and duck chose 
 
 
 if(computerChoice == 'rock'){
-computerPoints += 1;
+addPointsToComputer();
 computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerText = `Duck wins because ${playerName} chose scissor and duck choose rock`;
 }
 
 else if(computerChoice == 'paper'){
-humanPoints += 1;
+addPointsToHuman();
 humanPointText.innerHTML = `human points = ${humanPoints}`;
 message.innerText = `${playerName} wins because duck chose paper and ${playerName} chose scissor`;
 }
 
 else if(computerChoice == 'scissor'){
+addUnfairPointsWhenHardDifficulty();
+computerPointText.innerHTML = `Duck points = ${computerPoints}`;
 message.innerText = `it's a tie because both of you chose scissor`;
 }
 
@@ -127,8 +135,57 @@ document.body.appendChild(finalText);
 }
 
 
+function addPointsToHuman(){
+switch (computerDifficulty) {
+	case 'easy':
+		humanPoints += 2;
+		break;
+	case 'normal':
+		humanPoints += 1;
+		break;
+	case 'hard':
+		humanPoints += 0.75;
+		break;
+	case 'ultrahard':
+		humanPoints += 0.50;
+		break;
+	case 'almostImpossible':
+		humanPoints += 0.25;
+		break;
+}
+}
 
 
+function addPointsToComputer(){
+switch (computerDifficulty) {
+	case 'easy':
+		computerPoints += 0.75;
+		break;
+	case 'normal':
+		computerPoints += 1;
+		break;
+	case 'hard':
+		computerPoints += 1.5;
+		break;
+	case 'ultrahard':
+		computerPoints += 2.5;
+		break;
+	case 'almostImpossible':
+		computerPoints += 5;
+		break;
+}
+}
+
+function addUnfairPointsWhenHardDifficulty(){
+switch (computerDifficulty) {
+	case 'ultrahard':
+		computerPoints += 0.5;
+		break;
+	case 'almostImpossible':
+		computerPoints += 1;
+		break;
+}
+}
 
 
 
@@ -156,10 +213,54 @@ switch(e.target){
 	gameRules();
 }
 
+
+let computerDifficulty;
+const difficulty = document.querySelectorAll('#difficultySystem > *')
+
+
+
+function getDifficulty(e) {
+	console.log(e.target);
+	switch(e.target){
+	case difficulty[1]:
+			computerDifficulty = 'easy';
+			switchToTheGame();
+			break;
+	case difficulty[2]:
+			computerDifficulty = 'normal';
+			switchToTheGame();
+			break;
+	case difficulty[3]:
+			computerDifficulty = 'hard';
+			switchToTheGame();
+			break;
+	case difficulty[4]:
+			computerDifficulty = 'ultraHard';
+			switchToTheGame();
+			break;
+	case difficulty[5]:
+			computerDifficulty = 'almostImpossible';
+			switchToTheGame();
+			break;
+}
+}
+
+function switchToTheGame(){
+      document.querySelector('#difficultySystem').style.display = "none";
+      document.querySelector('#humanChoiceContainer').style.display = "flex";
+}
+
+
+
+
+
+
+
+let difficultySystem = document.querySelector('#difficultySystem');
+difficultySystem.addEventListener('click', getDifficulty)
+
 let invisibleContainer = document.querySelector('#invisibleContainer');
 invisibleContainer.addEventListener('click', playerChoice);
-
-
 
 PlayerNameDoneButton = document.querySelector('#nameInputContainer button');
 PlayerNameDoneButton.addEventListener('click', getPlayerName);
